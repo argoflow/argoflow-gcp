4b6dc2a887d7dfc3b25fe6e633d5d84a551dd51d
haoxin <haoxinst@gmail.com>
2021-08-12 11:05:32 +0800

    restructure - mv all the manifests into distribution directory

--- setup_repo.sh
+++ setup_repo.sh
@@ -2,14 +2,14 @@
 
 # set secretkey for metallb
 echo "generating secret for metallb"
-yq eval -i ".stringData.secretkey = \"$(openssl rand -base64 128)\"" metallb/secret.yaml
+yq eval -i ".stringData.secretkey = \"$(openssl rand -base64 128)\"" distribution/metallb/secret.yaml
 
 if [ -z "$1" ]
     then
         echo "no repo URL provided, using upstream"
     else
-        yq e -i ".spec.source.repoURL = \"$1\"" kubeflow.yaml
-        for filename in ./argocd-applications/*.yaml; do
+        yq e -i ".spec.source.repoURL = \"$1\"" distribution/kubeflow.yaml
+        for filename in ./distribution/argocd-applications/*.yaml; do
             yq e -i ".spec.source.repoURL = \"$1\"" $filename
         done
 fi
@@ -18,8 +18,8 @@ if [ -z "$2" ]
     then
         echo "no target branch provided, using HEAD"
     else
-        yq e -i ".spec.source.targetRevision = \"$2\"" kubeflow.yaml
-        for filename in ./argocd-applications/*.yaml; do
+        yq e -i ".spec.source.targetRevision = \"$2\"" distribution/kubeflow.yaml
+        for filename in ./distribution/argocd-applications/*.yaml; do
             yq e -i ".spec.source.targetRevision = \"$2\"" $filename
         done
 fi
