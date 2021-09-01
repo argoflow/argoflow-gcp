f546075c281473cea02a895d5e9505a500dc0fb7
Hao Xin <haoxinst@gmail.com>
2021-09-01 14:04:12 +0800

    update shell

--- setup_repo.sh
+++ setup_repo.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-    then
-        echo "no repo URL provided, using upstream"
-    else
-        yq e -i ".spec.source.repoURL = \"$1\"" distribution/kubeflow.yaml
-        for filename in ./distribution/argocd-applications/*.yaml; do
-            yq e -i ".spec.source.repoURL = \"$1\"" $filename
-        done
-fi
+# Perform a simple recursive find-and-replace on all variables defined in setup.conf
+export SETUP_CONF_PATH=$1 # location of the setup config
+export DISTRIBUTION_PATH=./distribution # folder where the distribution's YAML files are to be found
 
-if [ -z "$2" ]
-    then
-        echo "no target branch provided, using HEAD"
-    else
-        yq e -i ".spec.source.targetRevision = \"$2\"" distribution/kubeflow.yaml
-        for filename in ./distribution/argocd-applications/*.yaml; do
-            yq e -i ".spec.source.targetRevision = \"$2\"" $filename
-        done
-fi
+while IFS="=" read PLACEHOLDER VALUE # While loop that will perform simple parsing. On each line MY_VAR=123 will be read into PLACEHOLDER=MY_VAR, VALUE=123
+do
+  # recursively look for $PLACEHOLDER in all files in the $DISTRIBUTION_PATH and replace it with $VALUE
+  echo ${VALUE}
+  VALUE=$(echo "${VALUE////$'\/'}") #escape forward slashes (needed for sed to work correctly)
+  grep -rli ${PLACEHOLDER} ${DISTRIBUTION_PATH}/* | xargs -i@ sed -i "s/${PLACEHOLDER}/${VALUE}/g" @ #perform recursive replace
+done <${SETUP_CONF_PATH} # pass the setup config into the while loop
